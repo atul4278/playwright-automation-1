4f2f33f0ad1af4835457e544bc050d34ec57b5bd
Atul Jadhav <atul4278@gmail.com>
2023-09-26 18:38:34 +0530

    implented hooks for test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,17 +2,35 @@ import { test, expect } from '@playwright/test'
 import { LoginPage } from '../pages/loginPage'
 import { HomePage } from '../pages/homePage'
 
-test.only('error message shown for invalid credentials', async ({ page }) => {
-  const loginPage = new LoginPage(page)
-  await page.goto('http://localhost:8000/login')
-  await loginPage.login('pythonista', 'I<3testing1')
-  await loginPage.verifyErrorMessage('Invalid login! Please retry.')
-})
+test.describe('', () => {
+  test.beforeAll(() => {
+    console.log('Connecting to some DATABASE')
+  })
 
-test.only('successful login for valid credentials', async ({ page }) => {
-  const loginPage = new LoginPage(page)
-  const homePage = new HomePage(page)
-  await page.goto('http://localhost:8000/login')
-  await loginPage.login('pythonista', 'I<3testing')
-  await expect(homePage.logoutButton).toBeVisible()
+  test.beforeEach(async ({ page }) => {
+    console.log('Initializing browser setup')
+    await page.goto('http://localhost:8000/login')
+  })
+
+  test.afterEach(async ({ page }) => {
+    console.log('Tearing down browser setup')
+    await page.close()
+  })
+
+  test.afterAll(async () => {
+    console.log('Closing connection to DATABASE')
+  })
+
+  test.only('error message shown for invalid credentials', async ({ page }) => {
+    const loginPage = new LoginPage(page)
+    await loginPage.login('pythonista', 'I<3testing1')
+    await loginPage.verifyErrorMessage('Invalid login! Please retry.')
+  })
+
+  test.only('successful login for valid credentials', async ({ page }) => {
+    const loginPage = new LoginPage(page)
+    const homePage = new HomePage(page)
+    await loginPage.login('pythonista', 'I<3testing')
+    await expect(homePage.logoutButton).toBeVisible()
+  })
 })
